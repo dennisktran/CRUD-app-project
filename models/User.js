64843ae5a960776cca2cbf4681f74fd565dbb43d
Demarkus Matthews <dmanthony@Demarkuss-MacBook-Pro.local>
2019-07-25 15:38:29 -0700

    made modal

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    passwordcheck: {type: String, required: true},
+    email: {type: String, required: true},
+    videos: [{
+        type: mongoose.Schema.Types.ObjectId,  // ._id
+        ref: 'Video'
+      }],
+    likes: Number,
+    dislikes: Number,
+    pic: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -17,33 +17,19 @@ require('./db/db');
 
 app.use(session({
   secret: 'THIS IS A RANDOM SECRET STRING',
-  resave: false, // only save the cookie when, we
-  // add something or delete something from it
-  // mutate it
-  saveUninitialized: false // don't save the cookie
-  // until the user has "logged in" legal reasons
-  // as well, you're not supposed to track user data
-  // until the user has logged in to an app
+  resave: false, 
+  saveUninitialized: false 
 }));
 
-// set up the router at the end of the middleware chain,
-// Final Destination
-// I set up bodYParser before my routes because
-// we want to parse the request before it actually gets
-// to the route (be able to read req.body)
 app.use(bodyParser.urlencoded({extended:false}));
-// ?something=something
-// ?_method=PUT OR DELETE
 app.use(methodOverride('_method'));
 
 // app.use('/authors', authorsController);
 // app.use('/articles', articlesController);
 // app.use('/auth', usersController);
-
-// landing page we don't need a controller
+3
 app.get('/', (req, res) => {
   res.render('index.ejs', {
-    //message: req.session.message
   });
 });
 
