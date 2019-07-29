a8338decdbb62ab2ab2c2b1ddddb06a204906c07
celestialsky <trandennis0@gmail.com>
2019-07-28 21:37:31 -0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,10 +7,7 @@ router.post('/signup', userController.createUser);
 router.get('/logout', userController.logOut);
 router.get('/:id', userController.home);
 router.delete('/:id', userController.deleteUser);
-<<<<<<< HEAD
 router.put('/:id', userController.editUser);
-=======
-router.put('/:id/edit', userController.editUser);
->>>>>>> 81b27c8c3de5e1d633b6af899c2a1838dbc94d68
 
 module.exports = router;
+
