8c9e8c39b5435da0566bf906cb82ca9eb32d4886
Demarkus Matthews <dmanthony@Demarkuss-MacBook-Pro.local>
2019-07-29 10:25:36 -0700

    ready for dennis

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,26 +35,20 @@ module.exports = {
     home: async (req, res) => {
         try{
             const foundUser = await User.findById(req.params.id);
-            const getVideo = await Video.find({});
+            const userVideos = await User.findById(req.params.id).populate('videos');
             let mostLikes = 0;
-            let likedVideo = '';
-            for(let i = 0; i < getVideo.length; i++) {
-                if(getVideo[i].likes > mostLikes) 
-                mostLikes = getVideo[i].likes;
-                likedVideo = getVideo[i];
-            }
-            const userVideo = [];
-            for(let i = 0; i < foundUser.videos.length; i++) {
-                if(foundUser.videos[i].toString() === getVideo[i]._id.toString()) {
-                    userVideo.push(getVideo[i]);
-                }
+            let likedVideo = {};
+            for(let i = 0; i < userVideos.videos.length; i++) {
+                if(userVideos.videos[i].likes > mostLikes) 
+                mostLikes = userVideos.videos[i].likes;
+                likedVideo = userVideos.videos[i];
             }
             res.render('user/index.ejs', {
                 user: foundUser,
-                videos: userVideo,
+                userVideos: userVideos,
                 likedVideo: likedVideo
             });
-            console.log(userVideo)
+            console.log(userVideos)
             
         } catch(err) {
             console.log(err)
