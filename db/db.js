978f0e9c1fb76e51c1f6364bfc98b813b0dcfe87
Dennis Tran <trandennis0@gmail.com>
2019-08-01 14:14:12 -0700

    fixed mongoose connection string

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb://localhost/blog5" || process.env.MONGODB_URI
+const connectionString =  process.env.MONGODB_URI
 
 
 mongoose.connect(connectionString, {
